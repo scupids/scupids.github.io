214ca0424ee297f5d105f32c50003ff50eede22d
vipul261 <vipul261@gmail.com>
2015-10-02 21:09:09 +0530

    added basic html

--- config/environment.js
+++ config/environment.js
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL = '/name-of-your-repo';
+//    ENV.baseURL = '/name-of-your-repo';
   }
 
   return ENV;
