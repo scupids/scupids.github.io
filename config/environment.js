0a814a6de28c3c63f72ae9140ef945e5456f2a25
vipul261 <vipul261@gmail.com>
2015-10-02 21:35:14 +0530

    Added font in public folder

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'scupids',
     environment: environment,
-    baseURL: '/scupids',
+    baseURL: '/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
