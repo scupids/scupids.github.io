83448a6292385bd63a807f86fb8b5f3d61328fe3
vipul261 <vipul261@gmail.com>
2015-10-19 12:55:48 +0530

    Fixes JS-Hint errors, Added background color to cover incase image not loaded

--- app/components/landing-page.js
+++ app/components/landing-page.js
@@ -3,12 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['hero-container'],
     onInsert: function () {
-        $('.content').scroll(function(e){
+        var $ = window.$;
+        $('.content').scroll(function(){
             var scrollPosition = $('.content').scrollTop(),
                 scrollTop = $('#team').position().top;
 
             $('.nav-container').toggleClass('reveal', scrollPosition > scrollTop / 2);
-            $('.hero-content').toggleClass('reveal', !(scrollPosition > scrollTop / 3));
+            $('.hero-content').toggleClass('reveal', (scrollPosition <= scrollTop / 3));
             $('.member-container').toggleClass('reveal', (scrollPosition > 2*scrollTop / 3));
 
             return false;
--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['nav-container'],
     onInsert: function () {
+        var $ = window.$;
         $('[data-scroll]').click(function (e) {
             var href = $(e.target).attr('href'),
-                totalHeight = document.querySelector('.page-content').scrollHeight,
                 scrollPosition = $('.content').scrollTop(),
-                scrollTop = scrollPosition +$(href).position().top + 50
+                scrollTop = scrollPosition +$(href).position().top + 50;
             $('.content').animate({
                     scrollTop: Math.abs(scrollTop) -50
                 },'slow');
