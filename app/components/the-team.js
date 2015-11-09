15a2dc6375e50e04a33e262c3756dc2be2c9c76b
vipul261 <vipul261@gmail.com>
2015-11-09 14:27:29 +0530

    Adds compressed team images for faster load, Adds alt attr in images, fine-tunes down arrow animation, Adds meta description for SEO

--- app/components/the-team.js
+++ app/components/the-team.js
@@ -6,25 +6,25 @@ export default Ember.Component.extend({
             {
                 name: 'Abhishek Gupta',
                 id: 2,
-                img: '/imgs/team/abhishek.jpg',
+                img: '/imgs/team/400/abhishek.jpg',
                 desc: 'A nerdy creature, mostly found in solitude. Commonly known for his rotund frame and sophophilic brain. Known to mark his territory around any technology that comes his way.'
             },
             {
                 name: 'Niyati Agarwal',
                 id: 3,
-                img: '/imgs/team/niyati.jpg',
+                img: '/imgs/team/400/niyati.jpg',
                 desc: 'A fierce organism often seen on work desks, fidgeting with her hair or wrestling with the keyboard. Well acclaimed to work like an ant for stupendous hours, churning out robust strands of code doggedly.'
             },
             {
                 name: 'Pratik Jain',
                 id: 4,
-                img: '/imgs/team/pratik.jpg',
+                img: '/imgs/team/400/pratik.jpg',
                 desc: 'An observant being known to love sleep and daydreaming. When not hanging in the web of code, he is found navigating his herd through the travails  of this demanding world.'
             },
             {
                 name: 'Vipul Garg',
                 id: 1,
-                img: '/imgs/team/vipul.jpg',
+                img: '/imgs/team/400/vipul.jpg',
                 desc: 'A single dimensional creature, notable for his frail skeletal morphology and multifaceted creativity. Lives in the vicinity of charcoal, yet flaunts colorful fingers that can generate beautiful vistas on screen.'
 			}
         ];
